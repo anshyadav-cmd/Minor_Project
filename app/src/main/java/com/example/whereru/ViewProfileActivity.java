44ecac31918336a5a0974cc3b85a87d9157d3109
anshyadav <anshyadav4244@gmail.com>
2022-03-28 12:24:04 +0530

    packages and Activities added
    trackbus, viewbus, viewprofile activities and pacakages for recylerview

---
+++ app/src/main/java/com/example/whereru/ViewProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.whereru;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+    }
+}
