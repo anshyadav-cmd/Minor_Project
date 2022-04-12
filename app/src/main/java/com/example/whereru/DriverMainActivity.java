acebd81fad61a2b24b2382f25e183001f485d962
anshyadav <anshyadav4244@gmail.com>
2022-04-12 15:39:05 +0530

    driver logout button added

--- app/src/main/java/com/example/whereru/DriverMainActivity.java
+++ app/src/main/java/com/example/whereru/DriverMainActivity.java
@@ -1,14 +1,46 @@
 package com.example.whereru;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DriverMainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class DriverMainActivity extends AppCompatActivity {
+    private Button logoutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_main);
+
+        logoutBtn = findViewById(R.id.btnLogout);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder logoutAlert = new AlertDialog.Builder(DriverMainActivity.this)
+                        .setTitle("Are you sure you want to Logout?")
+                        .setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                FirebaseAuth.getInstance().signOut();
+                                startActivity(new Intent(getApplicationContext(), Login.class));
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", null);
+                logoutAlert.show();
+            }
+        });
     }
 }
